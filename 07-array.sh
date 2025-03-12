038cb3f1f801a1f5052be9d1a5fc1d562e35ae50
srinivasdevops97 <birudurajusrinivas@gmail.com>
2025-03-12 17:41:04 +0530

    variables

--- 07-array.sh
+++ 07-array.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#index starts from 0, sizeis 3
-FRUITS=("APPLE" "KIWI" "ORANGE")#array
+#index starts from 0, size is 3
+FRUITS=("APPLE" "KIWI" "ORANGE") #Array
 
-echo "first fruit is:${FRUITS[0]}"
-echo "second fruit is:${FRUITS[1]}"
-echo "thrid fruit is:${FRUITS[2]}"
+echo "first fruit is: ${FRUITS[0]}"
+echo "second fruit is: ${FRUITS[1]}"
+echo "thrid fruit is: ${FRUITS[2]}"
 
-echo "ALL fruits are:${FRUITS[@]}"
+echo "ALL fruits are: ${FRUITS[@]}"
