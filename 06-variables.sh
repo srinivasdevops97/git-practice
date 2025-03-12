2266896bb10abfdf9a2832d97bdebf2cf908051b
srinivasdevops97 <birudurajusrinivas@gmail.com>
2025-03-12 17:12:23 +0530

    variables

---
+++ 06-variables.sh
@@ -0,0 +1,8 @@
+#!/binbash
+
+echo "Please enter your username"
+read -s USERNAME
+echo "username entered is:$USERNAME"
+echo "Please enter your password"
+read -s PASSWORD
+echo "username entered is:$PASSWORD" 
