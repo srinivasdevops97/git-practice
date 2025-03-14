06859a71e18a9ea3a587d54904e0716ce7e89801
srinivasdevops97 <birudurajusrinivas@gmail.com>
2025-03-14 17:10:30 +0530

    variables

--- testing.sh
+++ testing.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
 USERID=$(id -u)
-echo "user id is:$USERID"
+#echo "user id is:$USERID"
+
+if [ $USER0ID -ne 0 ]
+then
+    echo "Please run the script with root privilages"
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "mysql is not installed, going to install it.."
+    dnf install mysql
+    if [ $? -ne 0 ]
+    then
+    echo"mysql is not installed, please check it"
+    exit 1
+    else
+    echo "mysql is installed successfully"
+    fi
+else
+    echo "mysql is already installed. Nothing to do.."
+fi
+    
