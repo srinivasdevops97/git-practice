f68b7b8942cec3428434a7608a4b792ba9651e14
srinivasdevops97 <birudurajusrinivas@gmail.com>
2025-03-15 18:12:52 +0530

    variables

--- 16-loops.sh
+++ 16-loops.sh
@@ -26,7 +26,7 @@ CHECK_ROOT
 # sh 16-loops.sh git mysql postfix nginx
 for $package in $@ #$@ refers to all arguments passed to it
 do
-    dnf list installed $p0ackage
+    dnf list installed $package
     if [ $? -ne 0 ]
     then
     echo "$package is not installed, going to install it"
