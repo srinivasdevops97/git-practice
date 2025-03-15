3db0a7d04c4cf1bde2f27f1990ee8813927eb01e
srinivasdevops97 <birudurajusrinivas@gmail.com>
2025-03-15 18:22:36 +0530

    variables

--- 16-loops.sh
+++ 16-loops.sh
@@ -4,27 +4,30 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
         echo "Please run the script with root privilages"
         exit 1
-fi
+    fi
 }
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is.. $R failed $N"
+        echo -e "$2 is.. $R FAILED $N"
         exit 1
     else
-        echo -e "$2 is.. $G success $N"
+        echo -e "$2 is.. $G SUCCESS $N"
     fi
 }
 
 
 CHECK_ROOT
+
 # sh 16-loops.sh git mysql postfix nginx
-for $Package in $@ #$@ refers to all arguments passed to it
+for Package in $@ #$@ refers to all arguments passed to it
 do
     dnf list installed $Package
     if [ $? -ne 0 ]
