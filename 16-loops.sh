92b7f3d009cccddb8b75b1f8686d2dae8059c52d
srinivasdevops97 <birudurajusrinivas@gmail.com>
2025-03-15 18:11:14 +0530

    variables

---
+++ 16-loops.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run the script with root privilages"
+        exit 1
+fi
+}
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is.. $R failed $N"
+        exit 1
+    else
+        echo -e "$2 is.. $G success $N"
+    fi
+}
+
+
+CHECK_ROOT
+# sh 16-loops.sh git mysql postfix nginx
+for $package in $@ #$@ refers to all arguments passed to it
+do
+    dnf list installed $p0ackage
+    if [ $? -ne 0 ]
+    then
+    echo "$package is not installed, going to install it"
+    dnf install $package -y
+    VALIDATE $? "Installing $package"
+    else
+    echo "$package is already installed, nothing to do"
+    fi
+done
