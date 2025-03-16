6a00fe0405d9765e88ac0252a77ae4b8b1425911
srinivasdevops97 <birudurajusrinivas@gmail.com>
2025-03-16 17:22:12 +0530

    variables

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -4,13 +4,5 @@ NUMBER1=$1
 NUMBER2=$2
 
 SUM=$(($NUMBER1+$NUMBER2))
-USAGE(){
-    echo -e "$R USAGE:: $N sudo sh 17-redirectors.sh Give number1 and number2"
-    exit 1
-}
-if [ $# -ne 0 ]
-then
-    USAGE
-fi
 
 echo "Total of $NUMBER1 and $NUMBER2 is:$SUM"
