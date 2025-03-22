3e411327fda45eb17e2c576bd79f727c749cb6e9
srinivasdevops97 <birudurajusrinivas@gmail.com>
2025-03-22 11:32:08 +0530

    variables

---
+++ practice.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+
+if [ USERID -ne 0 ]
+then
+    echo "Please run this script with root privileges"
+    exit 1
+fi
+
+dnf list installed git
+if [$? -ne 0 ]
+    then   
+        echo "git is not installed, going to install it"
+        dnf install git -y
+        if [ $? -ne 0 ]
+            then
+                echo "git installation is not successful, Please check"
+            else
+                echo "git is installed successfully"
+        fi
+    else
+        echo "git is already installed.. Nothing to do"
+fi
+
