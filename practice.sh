d239086cc01ea2c03c147e9cb6b3cc5b84b5cf14
srinivasdevops97 <birudurajusrinivas@gmail.com>
2025-03-24 12:01:59 +0530

    variables

--- practice.sh
+++ practice.sh
@@ -21,35 +21,22 @@ fi
 
 dnf list installed git
 
-VALIDATE $? "Listing git"
 
-# if [ $? -ne 0 ]
-#     then   
-#         echo "git is not installed, going to install it"
-#         dnf install git -y
-#         if [ $? -ne 0 ]
-#             then
-#                 echo "git installation is not successful, Please check"
-#                 exit 1
-#             else
-#                 echo "git is installed successfully"
-#         fi
-#     else
-#         echo "git is already installed.. Nothing to do"
-# fi
+if [ $? -ne 0 ]
+    then   
+        echo "git is not installed, going to install it"
+        dnf install git -y
+        VALIDATE $? "Installing git"
+    else
+        echo "git is already installed.. Nothing to do"
+fi
 
-# dnf list installed mysql
-# if [ $? -ne 0 ]
-#     then
-#         echo "mysql is not installed, going to install it"
-#         dnf install mysql -y  
-#         if [ $? -ne 0 ]
-#             then
-#                 echo "mysql installation is not successful, Please check"
-#                 exit 1
-#             else
-#                 echo "mysql is installed successfully"
-#         fi
-#     else
-#         echo "mysql is already installed.. Nothing to do"
-# fi
+dnf list installed mysql
+if [ $? -ne 0 ]
+    then
+        echo "mysql is not installed, going to install it"
+        dnf install mysql -y  
+        VALIDATE $? "Installing mysql"
+    else
+        echo "mysql is already installed.. Nothing to do"
+fi
