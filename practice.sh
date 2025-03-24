c536566f12d76a9d0cf189c9a7ed44ff381cf61a
srinivasdevops97 <birudurajusrinivas@gmail.com>
2025-03-24 19:08:24 +0530

    variables

--- practice.sh
+++ practice.sh
@@ -11,6 +11,7 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
 CHECK_ROOT(){
         if [ $USERID -ne 0 ]
@@ -33,24 +34,15 @@ VALIDATE (){
 CHECK_ROOT
 
 
-dnf list installed git
-
-
-if [ $? -ne 0 ]
-    then   
-        echo "git is not installed, going to install it"
-        dnf install git -y
-        VALIDATE $? "Installing git"
-    else
-        echo "git is already installed.. Nothing to do"
-fi
-
-dnf list installed mysql
-if [ $? -ne 0 ]
-    then
-        echo "mysql is not installed, going to install it"
-        dnf install mysql -y  
-        VALIDATE $? "Installing mysql"
-    else
-        echo "mysql is already installed.. Nothing to do"
-fi
+for Package in $@
+do
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]
+        then
+            echo "$package is not installed, going to install it" &>>$LOG_FILE
+            dnf install $package -y &>>$LOG_FILE
+            VALIDATE $? "Installing $package" 
+        else
+            echo "$package is already %Y installed, Nothing to do $N" &>>$LOG_FILE
+    fi  
+done
