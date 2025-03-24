96576695c9436647f8d178be3e61b97d9bbe6987
srinivasdevops97 <birudurajusrinivas@gmail.com>
2025-03-24 19:24:26 +0530

    variables

--- practice.sh
+++ practice.sh
@@ -2,7 +2,7 @@
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date-%Y-%m-%d-%H-%M-%S)
+TIMESTAMP=$(date+ %Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
@@ -38,7 +38,7 @@ USAGE(){
 
 CHECK_ROOT
 
-if [ $# -eq 0]
+if [ $# -eq 0 ]
 then
     USAGE
 fi
