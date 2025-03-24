471e8c106fc2eeeda3d70680b33a0bc344cea5a4
srinivasdevops97 <birudurajusrinivas@gmail.com>
2025-03-24 12:13:49 +0530

    variables

--- practice.sh
+++ practice.sh
@@ -3,21 +3,29 @@
 
 USERID=$(id -u)
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+        if [ $USERID -ne 0 ]
+            then
+                echo "Please run this script with root privileges"
+                exit 1
+        fi
+}
+
 VALIDATE (){
     if [ $1 -ne 0 ]
     then
-        echo "$2 is Failed"
+        echo -e "$2 is $R Failed $N"
         exit 1
     else
-        echo "$2 is Success"
+        echo "$2 is $G Success $N"
     fi
 }
 
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root privileges"
-    exit 1
-fi
+
 
 dnf list installed git
 
