2de3e536f990475cf9c867a71763b63f1fe781ac
srinivasdevops97 <birudurajusrinivas@gmail.com>
2025-03-24 19:13:05 +0530

    variables

--- practice.sh
+++ practice.sh
@@ -3,8 +3,8 @@
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date+%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-mkdir -p $LOG_FOLDER
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
 
 USERID=$(id -u)
 
