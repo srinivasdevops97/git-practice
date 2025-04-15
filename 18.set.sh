8474f54a1343730dbd6080bc8872248c2ec5d473
srinivasdevops97 <birudurajusrinivas@gmail.com>
2025-04-15 14:41:11 +0530

    expense-backend

--- 18.set.sh
+++ 18.set.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-set -ex #setting a automatic exit, if it gets a error
+set -ex #set -e-->setting a automatic exit, if it gets a error , set -ex--> for debug
 echo "Hello world Success"
 echooooo "Hello world failure"
 echo "Hello world After failure"
+
+#video paused at 1.16.38
