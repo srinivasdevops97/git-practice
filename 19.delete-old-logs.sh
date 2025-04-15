4882cac5e758fff3e76c7d6c19211b0a4c71fe4d
srinivasdevops97 <birudurajusrinivas@gmail.com>
2025-04-15 15:23:51 +0530

    expense-backend

--- 19.delete-old-logs.sh
+++ 19.delete-old-logs.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 SOURCE_DIR=/home/ec2-user/logs
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m
 
 if [ -d $SOURCE_DIR ]
 then
-    echo "$SOURCE_DIR exists"
+    echo -e "$G $SOURCE_DIR $N exists"
 else
-    echo "$SOURCE_DIR does not exists"
+    echo -e "$R $SOURCE_DIR $N does not exists"
     exit 1
 fi
