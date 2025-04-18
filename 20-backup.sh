3d6123f03df639ee85083d16bc6a1724e339c858
srinivasdevops97 <birudurajusrinivas@gmail.com>
2025-04-18 21:04:54 +0530

    expense-backend

--- 20-backup.sh
+++ 20-backup.sh
@@ -3,6 +3,7 @@
 SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14} #if $3 is empty, default is 14 days
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 
 R="\e[31m"
 G="\e[32m"
@@ -29,13 +30,30 @@ then
     echo "$DEST_DIR does not exist... please check"
 fi
 
-FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
 
 echo "Files: $FILES"
 
-if [ ! -z $FILES ] #true if FILES is empty
+if [ ! -z $FILES ] #true if FILES is empty, ! make it expression false
 then
     echo "Files are found"
+    ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    find ${SOURCE_DIR} -name "*.log" -mtime +14 | zip $ZIP_FILE -@
+
+    #check if zip file is successfully created or not
+    if [ -f $ZIP_FILE ]
+    then
+        echo "Successfully zipped files older than $DAYS"
+        #remove the files after zipping
+        while IFS= read -r file #IFS- internal field separator, empty means it will not ignore while spaces, -r is for not to ignore special characters like /
+        do
+            echo "Deleting file: $file"
+            rm -rf $file
+        done <<< $FILES
+    else
+        echo "Zipping the files is failed"
+        exit 1
+    fi
 else
     echo "NO files older than $DAYS"
 fi
