6d1ac39f5c685a9ef85de258f5a9d80f2369c217
srinivasdevops97 <birudurajusrinivas@gmail.com>
2025-04-19 19:53:57 +0530

    expense-backend

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -6,6 +6,10 @@ DISK_THRESHOLD=5 # in real projects, it is usually 75
 
 while IFS= read -r line #IFS- internal field separator, empty means it will not ignore while spaces, -r is for not to ignore special characters like /
 do
-    echo $line
-    rm -rf $file
+    USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        echo "$PARTITION is more than $DISK_THRESHOLD, current value: $USAGE. Please check"
+    fi
 done <<< $DISK_USAGE
